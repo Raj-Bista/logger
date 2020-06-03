f13c20eff0529339eb2f6fe80f3ee1b96942ff06
rails course from udemy <lomaslove132@gmail.com>
2020-06-03 10:57:04 +1000

    redux set up actions reducers and store

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
 import TechListModal from './components/techs/TechListModal';
+import {Provider} from 'react-redux';
+import store from './store';
 
 function App() {
   useEffect(()=>{
@@ -17,6 +19,7 @@ function App() {
     M.AutoInit();
   })
   return (
+    <Provider store={store}>
      <Fragment>
        <SearchBar />
        <div className="container">
@@ -28,6 +31,7 @@ function App() {
          <TechListModal />
        </div>
      </Fragment>
+     </Provider>
   );
 }
 
